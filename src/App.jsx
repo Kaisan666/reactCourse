9484a74421c46f0ef3e691c26fe8ae2dc9105b92
Sasha <sasha41187@gmail.com>
2025-10-30 11:25:07 +0300

    react practice init commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Greet from "./Greet"
+
+function App() {
+  return (
+  <>
+  <Greet/>
+  </>
+  )
+}
+
+export default App
---
+++ src/Greet.jsx
@@ -0,0 +1,5 @@
+function Greet(){
+    return <h1>hello</h1>
+}
+
+export default Greet
