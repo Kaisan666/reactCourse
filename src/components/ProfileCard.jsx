32d8b01a4e73239a98f5f8e3dea5a7e1e1a756ef
Alexander <sasha41187@gmail.com>
2025-11-02 17:26:24 +0300

    asdas

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfileCard = () => {
+    const styles = {backgroundColor: "lightGray", padding: "15px", borderRadius: "8px", color: "black"}
+  return (
+    <div style={styles}>
+        <p>adpoaskdpasoikas</p>
+        <p>adpoaskdpasoikas</p>
+    </div>
+  )
+}
+
+export default ProfileCard
---
+++ src/components/StyledCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const StyledCard = () => {
+  
+    return (
+    <div style={{backgroundColor : "lightblue", padding: "20px", borderRadius : "10px", color: "darkblue"}}>
+        <p>asdopaskdsaopkd</p>
+        <p>asdopaskdsaopkd</p>
+        <p>asdopaskdsaopkd</p>
+        <p>asdopaskdsaopkd</p>
+    </div>
+  )
+}
+
+export default StyledCard
